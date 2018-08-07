b02f82ad06db06d9490bad361c067cc23f8c1c45
Levi Bernard <levichristianbernard@gmail.com>
2018-08-06 23:05:22 -0700

    added dev dependencies

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const express = require('express');
+"use strict";
+
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.listen(process.env.PORT || 8080);
