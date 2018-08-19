ceaaefba86633d3c7a8bf2a4d977420662359ed8
Levi Bernard <levichristianbernard@gmail.com>
2018-08-18 17:00:40 -0700

    added .travis.yml file and began working on tests

--- test/test-travel-log-posts.js
+++ test/test-travel-log-posts.js
@@ -12,21 +12,39 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe("server response", function() {
-  before(function() {
-    return runServer();
-  });
-
-  after(function() {
-    return closeServer();
-  });
-
-  it("should return 400", function() {
+describe("plans", function() {
+  // before(function() {
+  //   return runServer();
+  // });
+  // after(function() {
+  //   return closeServer();
+  // });
+  // `chai.request.get` is an asynchronous operation. When
+  // using Mocha with async operations, we need to either
+  // return an ES6 promise or else pass a `done` callback to the
+  // test that we call at the end. We prefer the first approach, so
+  // we just return the chained `chai.request.get` object.
+  it("should list plans on GET", function() {
     return chai
       .request(app)
-      .get("http://localhost:8080")
+      .get("/plans")
       .then(function(res) {
-        res.should.have.status(400);
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("string");
+        expect(res.body.length).to.be.above(0);
+        res.body.forEach(function(item) {
+          expect(item).to.be.a("object");
+          expect(item).to.have.all.keys(
+            "id",
+            "title",
+            "seasonToGO",
+            "description",
+            "currency",
+            "words",
+            "todo"
+          );
+        });
       });
   });
 });
