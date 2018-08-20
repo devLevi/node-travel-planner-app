9aa970d49d0a569bafcdbc27db5dc2e61cdb9806
Levi Bernard <levichristianbernard@gmail.com>
2018-08-20 08:17:30 -0700

    formating changes

--- users/router.js
+++ users/router.js
@@ -22,7 +22,7 @@ router.post('/', jsonParser, (req, res) => {
         });
     }
 
-    const stringFields = ['username', 'password', 'firstName', 'lastName'];
+    const stringFields = ['username', 'password'];
     const nonStringField = stringFields.find(
         field => field in req.body && typeof req.body[field] !== 'string'
     );
@@ -94,11 +94,8 @@ router.post('/', jsonParser, (req, res) => {
         });
     }
 
-    let { username, password, firstName = '', lastName = '' } = req.body;
-    // Username and password come in pre-trimmed, otherwise we throw an error
-    // before this
-    firstName = firstName.trim();
-    lastName = lastName.trim();
+    let { username, password } = req.body;
+    // Username and password come in pre-trimmed
 
     return User.find({ username })
         .count()
@@ -118,9 +115,7 @@ router.post('/', jsonParser, (req, res) => {
         .then(hash => {
             return User.create({
                 username,
-                password: hash,
-                firstName,
-                lastName
+                password: hash
             });
         })
         .then(user => {
