bad6dd87418a4f4b5ea68d406ee0448ac7b364a5
Levi Bernard <levichristianbernard@gmail.com>
2018-08-20 21:48:43 -0700

    added gitingore file

--- public/app/store.js
+++ public/app/store.js
@@ -1,35 +1,35 @@
 let store = [
-  {
-    _id: "111111111111111111111101",
-    title: "Costa Rica",
-    seasonToGo: "mid-December-April",
-    description:
-      "Costa Rica is small country in Central America. It is bordered by Nicaragua to the north and Panama to the south. The Caribbean Sea is to the east and the Pacific Ocean is to the west. Costa Rica is slightly smaller than the state of West Virginia.",
-    currency: "Costa Rican Colón: 1 Colón = 0.0018 USD",
-    words:
-      "Pura vida: “Pure life”, Brete: “Work”, Llevarla suave: “To take it easy”",
-    todo: "Visit Arenal Valcano"
-  },
-  {
-    _id: "111111111111111111111102",
-    title: "Costa Rica",
-    seasonToGo: "mid-December-April",
-    description:
-      "Costa Rica is small country in Central America. It is bordered by Nicaragua to the north and Panama to the south. The Caribbean Sea is to the east and the Pacific Ocean is to the west. Costa Rica is slightly smaller than the state of West Virginia.",
-    currency: "Costa Rican Colón: 1 Colón = 0.0018 USD",
-    words:
-      "Pura vida: “Pure life”, Brete: “Work”, Llevarla suave: “To take it easy”",
-    todo: "Visit Arenal Valcano"
-  },
-  {
-    _id: "111111111111111111111103",
-    title: "Costa Rica",
-    seasonToGo: "mid-December-April",
-    description:
-      "Costa Rica is small country in Central America. It is bordered by Nicaragua to the north and Panama to the south. The Caribbean Sea is to the east and the Pacific Ocean is to the west. Costa Rica is slightly smaller than the state of West Virginia.",
-    currency: "Costa Rican Colón: 1 Colón = 0.0018 USD",
-    words:
-      "Pura vida: “Pure life”, Brete: “Work”, Llevarla suave: “To take it easy”",
-    todo: "Visit Arenal Valcano"
-  }
+    {
+        _id: '111111111111111111111101',
+        title: 'Costa Rica',
+        seasonToGo: 'mid-December-April',
+        description:
+            'Costa Rica is small country in Central America. It is bordered by Nicaragua to the north and Panama to the south. The Caribbean Sea is to the east and the Pacific Ocean is to the west. Costa Rica is slightly smaller than the state of West Virginia.',
+        currency: 'Costa Rican Colón: 1 Colón = 0.0018 USD',
+        words:
+            'Pura vida: “Pure life”, Brete: “Work”, Llevarla suave: “To take it easy”',
+        todo: 'Visit Arenal Valcano'
+    },
+    {
+        _id: '111111111111111111111102',
+        title: 'Costa Rica',
+        seasonToGo: 'mid-December-April',
+        description:
+            'Costa Rica is small country in Central America. It is bordered by Nicaragua to the north and Panama to the south. The Caribbean Sea is to the east and the Pacific Ocean is to the west. Costa Rica is slightly smaller than the state of West Virginia.',
+        currency: 'Costa Rican Colón: 1 Colón = 0.0018 USD',
+        words:
+            'Pura vida: “Pure life”, Brete: “Work”, Llevarla suave: “To take it easy”',
+        todo: 'Visit Arenal Valcano'
+    },
+    {
+        _id: '111111111111111111111103',
+        title: 'Costa Rica',
+        seasonToGo: 'mid-December-April',
+        description:
+            'Costa Rica is small country in Central America. It is bordered by Nicaragua to the north and Panama to the south. The Caribbean Sea is to the east and the Pacific Ocean is to the west. Costa Rica is slightly smaller than the state of West Virginia.',
+        currency: 'Costa Rican Colón: 1 Colón = 0.0018 USD',
+        words:
+            'Pura vida: “Pure life”, Brete: “Work”, Llevarla suave: “To take it easy”',
+        todo: 'Visit Arenal Valcano'
+    }
 ];
