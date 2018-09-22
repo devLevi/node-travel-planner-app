4540fa54c8dd732e77d988da17285cfa465ccac3
Levi Bernard <levichristianbernard@gmail.com>
2018-09-22 08:50:01 -0700

    uncommented proces.env.DATABASE_URL

--- config.js
+++ config.js
@@ -3,8 +3,7 @@
 module.exports = {
     PORT: process.env.PORT || 8080,
     DATABASE_URL:
-        // process.env.DATABASE_URL ||
-        'mongodb://localhost:27017/travel-app',
+        process.env.DATABASE_URL || 'mongodb://localhost:27017/travel-app',
     TESTDATABASE_URL:
         process.env.TESTDATABASE_URL ||
         'mongodb://localhost:27017/test-travel-app',
